678cc012127e536c4d518e2d60071f2d9fbb519e
Akki4297 <120674208+Akki4297@users.noreply.github.com>
2024-06-15 00:15:09 +0530

    Time: 69 ms (16.50%) | Memory: 16.8 MB (86.17%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -17,16 +17,21 @@ class Solution:
         root1.right = self.mergeTrees(root1.right,root2.right)
         
         return root1
-        '''if not root1 and not root2:
-            return None
-        if not root1:
+        '''if not root1 and root2:
             return root2
-        if not root2:
-            return root1
-
-        root1.val += root2.val
-
-        root1.left = self.mergeTrees(root1.left, root2.left)
-        root1.right = self.mergeTrees(root1.right, root2.right)
-
+        def merge(root1,root2):
+            if root1 and root2:
+                root1.val = root1.val + root2.val
+                if root1.left or root2.left:
+                    if not root1.left and root2.left:
+                        root1.left = root2.left
+                    else:
+                        merge(root1.left,root2.left)
+                if root1.right or root2.right:
+                    if not root1.right and root2.right:
+                        root1.right = root2.right
+                    else:
+                        merge(root1.right,root2.right)
+            return
+        merge(root1,root2)
         return root1'''
