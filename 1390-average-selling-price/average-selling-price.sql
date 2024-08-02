af4d34227c23538bf7b73f252a5879a5cbc8a6a3
Akki4297 <120674208+Akki4297@users.noreply.github.com>
2024-08-02 09:13:58 +0530

    Time: 726 ms (47.00%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,3 @@
 Select P.product_id, ifnull(Round(sum(price*units)/sum(units),2),0) As average_price
-from Prices AS P left join UnitsSold AS U on P.product_id = U.product_id 
-Where (U.purchase_date BETWEEN P.start_date AND P.end_date) or U.purchase_date is null
+from Prices P left join UnitsSold U on P.product_id = U.product_id And (U.purchase_date BETWEEN P.start_date AND P.end_date)
 Group by P.product_id;
